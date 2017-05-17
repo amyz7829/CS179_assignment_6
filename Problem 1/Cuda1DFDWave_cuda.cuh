42ea7fdc60f4be7d1093db4bf3cb058166a91dc5
amyz7829 <amyz7829@gmail.com>
2017-05-16 22:48:35 -0700

    asdf

--- Problem 1/Cuda1DFDWave_cuda.cuh
+++ Problem 1/Cuda1DFDWave_cuda.cuh
@@ -10,6 +10,7 @@
 /* TODO: This is a CUDA header file.
 If you have any functions in your .cu file that need to be
 accessed from the outside, declare them here */
-
+void cudaCallDisplacementKernel(const unsigned int blocks, const unsigned int threadsPerBlock,
+  const float *old_d, const float *current_d, float *new_d, float size, float courant);
 
 #endif // CUDA_1D_FD_WAVE_CUDA_CUH
