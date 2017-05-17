fbf02fed34519e4a1bfcb537a95752436d5aeb41
amyz7829 <amyz7829@gmail.com>
2017-05-16 22:42:17 -0700

    w

--- Problem 1/Cuda1DFDWave_cuda.cu
+++ Problem 1/Cuda1DFDWave_cuda.cu
@@ -21,6 +21,7 @@ void cudaDisplacementKernel(const float *old_d, const float *current_d, float *n
   }
 }
 
-void cudaCallDisplacementKernel(const unsigned int blocks, const unsigned int threadsPerBlock, const float *old_d, const float *current_d, float *new_d, float size float courant){
+void cudaCallDisplacementKernel(const unsigned int blocks, const unsigned int threadsPerBlock,
+  const float *old_d, const float *current_d, float *new_d, float size float courant){
   cudaDisplacementKernel<<<blocks, threadsPerBlock>>>(old_d, current_d, new_d, courant);
 }
