2ecf4df79683fa865223371961090e622a11b679
amyz7829 <amyz7829@gmail.com>
2017-05-16 22:41:18 -0700

    asf

--- Problem 1/Cuda1DFDWave_cuda.cu
+++ Problem 1/Cuda1DFDWave_cuda.cu
@@ -15,12 +15,12 @@ to call it */
 void cudaDisplacementKernel(const float *old_d, const float *current_d, float *new_d, float size, float courant){
   int tid = threadIdx.x;
   int idx = blockIdx.x * blockDim.x + tid;
-  while(idx < numberOfNodes - 2){
+  while(idx < size - 2){
     new_d[idx] = 2 * current_d[idx] - old_d[idx] + courant * courant * (current_d[idx + 1] - 2 * current_d[idx] + current_d[idx - 1]);
     idx += blockDim.x + gridDim.x;
   }
 }
 
-void cudaCallDisplacementKernel(const unsigned int blocks, const unsigned int threadsPerBlock, const float *old_d, const float *current_d, const float *new_d, float courant){
+void cudaCallDisplacementKernel(const unsigned int blocks, const unsigned int threadsPerBlock, const float *old_d, const float *current_d, float *new_d, float size float courant){
   cudaDisplacementKernel<<<blocks, threadsPerBlock>>>(old_d, current_d, new_d, courant);
 }
